3d46f88747a13a22435dc77c0e4ea962b518ccc7
kirilleitan <kirilleitan@gmail.com>
2020-12-24 21:32:11 +0200

    add react router dom

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames'
+
+function Button({onClick,className,outline,children}) {
+    return (
+        <div>
+            <button onClick={onClick} className={classNames('button',className,
+                {'button--outline':outline},
+            )}>{children}</button>
+        </div>
+    )
+}
+
+export default Button;
+
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Header from "./Header";
+import Categories from "./Categories";
+import SortPopup from "./SortPopup"
+
+//export {default as Button} from './Button'
+
+export {Button,Header,Categories,SortPopup}
