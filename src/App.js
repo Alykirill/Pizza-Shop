acf3f6940a8115e7fa9884e5e8bf1f9b4f356d57
kirilleitan <kirilleitan@gmail.com>
2021-01-14 15:21:02 +0200

    new functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React,{useEffect,useState} from "react";
+import React, {useEffect} from "react";
 import {Header} from "./components";
 import {Cart, Home} from "./pages";
 import {Route} from 'react-router-dom'
 import axios from "axios";
-import store from "./redux/store";
 import {setPizzas} from "./redux/actions/pizzas";
-import {connect, useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 
 
 function App(){
-
     const dispatch = useDispatch();
-
 
 
     useEffect(() =>{
@@ -26,7 +23,7 @@ function App(){
             <div className="wrapper">
                 <Header/>
                 <div className="content">
-                    <Route exact path='/' render={() => <Home items={items}/>}/>
+                    <Route exact path='/' component={Home}/>}/>
                     <Route exact path='/cart' component={Cart}/>
                 </div>
             </div>
