d1660da93717c257d8ed80faf8accac36fd94a9d
kirilleitan <kirilleitan@gmail.com>
2021-01-17 23:17:02 +0200

    add is loading and sort logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,60 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-import {Categories, PizzaBlock, SortPopup} from '../components';
+import { Categories, SortPopup, PizzaBlock, PizzaLoadingBlock } from '../components';
 
-import {setCategory} from '../redux/actions/filters';
-import {fetchPizzas} from '../redux/actions/pizzas';
-import {useEffect} from "react/cjs/react.production.min";
+import { setCategory, setSortBy } from '../redux/actions/filters';
+import { fetchPizzas } from '../redux/actions/pizzas';
 
-const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
-const sortItems = [
-    {name: 'популярности', type: 'popular'},
-    {name: 'цене', type: 'price'},
-    {name: 'алфавит', type: 'alphabet'},
-]
+const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+const sortIems = [
+  { name: 'популярности', type: 'popular', order: 'desc' },
+  { name: 'цене', type: 'price', order: 'desc' },
+  { name: 'алфавит', type: 'name', order: 'asc' },
+];
 
 function Home() {
-    const dispatch = useDispatch();
-    const items = useSelector(({pizzas}) => pizzas.items)
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
+  const { category, sortBy } = useSelector(({ filters }) => filters);
 
-    useEffect(() =>{
-        dispatch(fetchPizzas())
-    },[])
+  React.useEffect(() => {
+    dispatch(fetchPizzas(sortBy, category));
+  }, [category, sortBy]);
 
-    const onSelectCategory = React.useCallback((index) => {
-        dispatch(setCategory(index))
-    }, []);
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
 
-    return (
-        <div className="container">
-            <div className="content__top">
-                <Categories
-                    onClickItem={onSelectCategory}
-                    items={categoryNames}
-                />
-                <SortPopup
-                    items={sortItems}
-                />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {items && items.map((obj) => (
-                    <PizzaBlock key={obj.id} {...obj} />
-                ))}
-            </div>
-        </div>
-    );
+  const onSelectSortType = React.useCallback((type) => {
+    dispatch(setSortBy(type));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          activeCategory={category}
+          onClickCategory={onSelectCategory}
+          items={categoryNames}
+        />
+        <SortPopup
+          activeSortType={sortBy.type}
+          items={sortIems}
+          onClickSortType={onSelectSortType}
+        />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoaded
+          ? items.map((obj) => <PizzaBlock key={obj.id} isLoading={true} {...obj} />)
+          : Array(12)
+              .fill(0)
+              .map((_, index) => <PizzaLoadingBlock key={index} />)}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,11 @@
-import filtersReducer from "./filters";
-import pizzasReducer from "./pizzas";
-import {combineReducers} from "redux";
+import { combineReducers } from 'redux';
+
+import filtersReducer from './filters';
+import pizzasReducer from './pizzas';
 
 const rootReducer = combineReducers({
-    pizzas:pizzasReducer,
-    filters:filtersReducer
-})
+  filters: filtersReducer,
+  pizzas: pizzasReducer,
+});
 
-
-
-export default rootReducer
+export default rootReducer;
